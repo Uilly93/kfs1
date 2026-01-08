ed9a075369fd9745abf09b1ec03266692cb2582d
William Nocchi <wnocchi@f6r2s9.clusters.42paris.fr>
2026-01-08 19:00:28 +0100

    makefile

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 apt-get update && apt-get install -y gcc-i686-linux-gnu binutils-i686-linux-gnu
-i686-linux-gnu-as boot.s -o boot.o
-i686-linux-gnu-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-linux-gnu-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib -fno-builtin -fno-exceptions -fno-stack-protector -fno-rtti -nostdlib -nodefaultlibs boot.o kernel.o -lgcc
+i686-linux-gnu-as src/boot.s -o boot.o
+i686-linux-gnu-gcc -c src/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -ffreestanding -O2 -nostdlib -fno-builtin -fno-exceptions -fno-stack-protector -nostdlib -nodefaultlibs -fno-pie
+i686-linux-gnu-gcc -T src/linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc -no-pie
+# i686-linux-gnu-objdump -h myos.bin
